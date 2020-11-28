c44bcb1ba834e4e97f0dab40617d654bbf51008f
Tharak <tharakzn@gmail.com>
2020-11-28 14:17:27 -0300

    Initial Commit

---
+++ LegendOfTheFiveRingsApp/LegendOfTheFiveRingsAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  LegendOfTheFiveRingsAppApp.swift
+//  LegendOfTheFiveRingsApp
+//
+//  Created by Tharak on 28/11/20.
+//
+
+import SwiftUI
+
+@main
+struct LegendOfTheFiveRingsAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
